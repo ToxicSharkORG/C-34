e536764b2a5309c79e7e6537228449c0bf22ea6b
ToxicSharkORG <73337206+ToxicSharkORG@users.noreply.github.com>
2021-03-13 15:06:40 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,34 @@
+class Ball {
+    constructor(x, y,radius) {
+      var options = {
+          
+          'friction':1.0,
+          'density':2,
+      }
+      this.body = Bodies.circle(x, y, radius, options);
+      this.radius = radius;
+            
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+
+      rotate(angle);
+      
+      ellipseMode(RADIUS);
+
+
+      strokeWeight(4);
+      
+      stroke("yellow");
+      
+      fill("black");
+      
+      ellipse(0, 0, this.radius,this.radius);
+      
+      pop();
+    }
+  }
